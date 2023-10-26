0b62b48468adf6b7f4f57a5d5276049f46309da0
der060738 <212417025@o365.tku.edu.tw>
2023-10-26 19:35:51 +0800

    ### W07-P1: Show classdemo of week 2 and week 7

---
+++ demo/script_25.js
@@ -0,0 +1,24 @@
+function showdemo(week) {
+    const p = document.querySelector('.show-classdemo')
+    console.log('p', p);
+    switch(week){
+        case 2:
+          p.innerHTML = `<iframe src="./w02_25_intro/index.html" width="100%" height="100%" />`;
+          break;
+        case 3:
+
+          break;
+        case 4:
+
+          break;
+        case 5:
+
+          break;
+        case 6:
+          p.innerHTML = `<iframe src="./w06_25_navbar/index.html" width="100%" height="100%" />`;
+          break;
+        case 7:
+
+          break;
+    }
+}
