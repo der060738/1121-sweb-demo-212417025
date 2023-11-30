2a765fc0a3d23206740de3b35f3fad750823e24d
der060738 <212417025@o365.tku.edu.tw>
2023-11-30 20:38:49 +0800

    W12-P3: Create sidebar by pressing nav-btn, remove sidebar by pressing close-btn

--- demo/w12_portfolio_25/portfolio_25/js/script_25.js
+++ demo/w12_portfolio_25/portfolio_25/js/script_25.js
@@ -1,5 +1,19 @@
 const navbar = document.querySelector('#nav');
 const date = document.querySelector('#date');
+
+
+const navBtn = document.querySelector('.nav-btn');
+const sidebar = document.querySelector('#sidebar');
+const closeBtn = document.querySelector('#close-btn');
+
+navBtn.addEventListener('click', function () {
+   sidebar.classList.add('show-sidebar');
+});
+
+closeBtn.addEventListener('click', function() {
+  sidebar.classList.remove('show-sidebar');
+});
+
 // add fixed class to navbar
 window.addEventListener('scroll', function () {
   if (window.scrollY > 80) {
