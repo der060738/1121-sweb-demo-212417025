cef122a186bc3b6f7c1a9e93daac86973fd29f92
der060738 <212417025@o365.tku.edu.tw>
2023-12-07 22:12:35 +0800

    W13-P5: Use mega menu theme to show w03-card page

---
+++ demo/w13_mega_menu_theme_25/script_25.js
@@ -0,0 +1,24 @@
+function showdemo(week) {
+  const p = document.querySelector('.show-classdemo')
+  console.log('p', p);
+  switch(week){
+      case 2:
+        p.innerHTML = `<iframe src="../w02_25_intro/index.html" width="100%" height="100%" />`;
+        break;
+      case 3:
+        p.innerHTML = `<iframe src="../w03_25_card/index.html" width="100%" height="100%" />`;
+        break;
+      case 4:
+        p.innerHTML = `<iframe src="../w04_25_blog/index.html" width="100%" height="100%" />`;
+        break;
+      case 5:
+        p.innerHTML = `<iframe src="../w05_25_blogs/index.html" width="100%" height="100%" />`;
+        break;
+      case 6:
+        p.innerHTML = `<iframe src="../w06_25_navbar/index.html" width="100%" height="100%" />`;
+        break;
+      case 7:
+        p.innerHTML = `<iframe src="../index.html" width="100%" height="100%" />`;
+        break;
+  }
+}
